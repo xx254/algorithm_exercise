e801378a50a6d4288f7792426ac06e5f295c6c61
xx254 <xx254@users.noreply.github.com>
2017-09-28 23:29:59 -0400

    Update A_Copy_List_With_Random_Pointer.java

--- others/A_Copy_List_With_Random_Pointer.java
+++ others/A_Copy_List_With_Random_Pointer.java
@@ -29,3 +29,7 @@ public class Solution {
         return h.get(dummy.next);
     }
 }
+
+// 第二种方法：
+每一个node，new出一个newNode，并且 newNode 的 random 和 next 都指向原来 node 的 random 和 next。并把 newNode 加在原来 node 之后。
+接下来把 random 归位到应有的 random，并把所有新 node 取出连起来（即 next 归位到应有的 next）。
